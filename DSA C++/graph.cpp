08405100d2e5b2f425c98f0ac3445507a9b7572e
08Miguel24 <MichaelAveuc571@gmail.com>
2025-03-07 18:09:27 +0800

    updated from shell

--- DSA C++/graph.cpp
+++ DSA C++/graph.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 class Graph{
     private:
@@ -23,27 +24,82 @@ class Graph{
     public:
         int m; 
         int n;
+        bool** visited;
         Graph(){
             // dimensions of graph
             m = sizeof(board) / sizeof(board[0]);
             n = sizeof(board[0]) / sizeof(board[0][0]);
+
+            // initialize visited matrix to false values
+            // as this we will use to track if a nodes coordinate
+            // has been visited indicated as a true value and false
+            // otherwise
+            visited = new bool*[m];
+            for(int i = 0; i < m; i++){
+                visited[i] = new bool[n];
+            }
+
+            // fill in newly declared matrix with false values
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    visited[i][j] = false;
+                }
+            }
         }
 
-        int findShortestPath(){
+        int findShortestPath(int* start, int* end){
             /*
             Given a 15x15 board with cells '0' (open path), '1' (obstacle), 'X’ (starting point), 
             and 'FLAG' (destination), make a python code that will find the shortest path from a 
             start cell (‘X') to 'FLAG. Output should be the coordinates of the 'X' with the 
             shortest path to 'FLAG’.
             */
-            std::cout << m << " " << n;
+
+            int start_r = start[0], start_c = start[1];
+            int end_r = end[0], end_c = end[1];
+            int reached_end = false;
+
+            // row queue and column queue to 
+            std::queue<int> rq;
+            std::queue<int> cq;
+            int curr_r, curr_c;
+
+            // for(int i = 0; i < m; i++){
+            //     for(int j = 0; j < n; j++){
+            //         std::cout << visited[i][j];
+            //     }
+            // }
+            rq.push(start_r);
+            cq.push(start_c);
+
+            while(rq.size() > 0){
+                // get front element before popping out of the queue
+                curr_r = rq.front();
+                curr_c = cq.front();
+
+                // pop the current coordinates
+                rq.pop();
+                cq.pop();
+                
+                // check if current coordinates value matches the
+                // value of the end coordinate which would be F
+                if(board[curr_r][curr_r] == 'X'){
+                    reached_end = true;
+                    break;
+                }
+            }
+            std::cout << curr_r << " " << curr_c;
+            // std::cout << m << " " << n;
         }
 };
 
 
 int main(int argc, char** argv){
     Graph g;
-    g.findShortestPath();
+    int start[] = {14, 0};
+    int end[] = {0, 14};
+
+    g.findShortestPath(start, end);
 
     return 0;
 }
